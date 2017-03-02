4914b060850ec524235dbec9343bd7aa84bf81c3
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2017-03-02 16:18:42 +0000

    Added class to handle DB

---
+++ Server/src/main/java/pt/tecnico/sec/dpm/server/db/DBManager.java
@@ -0,0 +1,60 @@
+package pt.tecnico.sec.dpm.server.db;
+
+import java.sql.*;
+
+public class DBManager {
+	private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	
+	private Connection conn = null;
+	
+	public DBManager(String url, String username, String password) {
+		// Register driver
+		try {
+			Class.forName(JDBC_DRIVER);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// Open connection
+		try {
+			conn = DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	// TODO: Throw the exception instead, but create a package for all the exceptions on the server side!!!
+	
+	
+	// To make a DB select query
+	private ResultSet select(String q) {
+		ResultSet res = null;
+		try {
+			Statement stmt = conn.createStatement();
+			res = stmt.executeQuery(q);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return res;
+	}
+		
+	// To insert data into the tables
+	private void insert(String q) {		
+		try {
+			Statement stmt = conn.createStatement();
+			stmt.executeUpdate(q);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	// To update table records
+	private void update(String q) {		
+		insert(q);
+	}
+}
--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Maven
-cd Tools
-unzip apache-maven-3.3.9-bin.zip 'apache-maven-3.3.9/*' -d $HOME
-export PATH=$HOME/apache-maven-3.3.9/bin:$PATH
-cd ..
+#cd Tools
+#unzip apache-maven-3.3.9-bin.zip 'apache-maven-3.3.9/*' -d $HOME
+#export PATH=$HOME/apache-maven-3.3.9/bin:$PATH
+#cd ..
 
 # MySQL
-sudo apt-get update
-sudo apt-get install mysql-server
-sudo mysql_secure_installation
+#sudo apt-get update
+#sudo apt-get install mysql-server
+#sudo mysql_secure_installation
 
 # jUDDI
-cd Tools
-unzip juddi-3.3.2_tomcat-7.0.64_9090.zip 'juddi-3.3.2_tomcat-7.0.64_9090/*' -d $HOME
-chmod +x $HOME/juddi-3.3.2_tomcat-7.0.64_9090/bin/*.sh
-cd ..
+#cd Tools
+#unzip juddi-3.3.2_tomcat-7.0.64_9090.zip 'juddi-3.3.2_tomcat-7.0.64_9090/*' -d $HOME
+#chmod +x $HOME/juddi-3.3.2_tomcat-7.0.64_9090/bin/*.sh
+#cd ..
