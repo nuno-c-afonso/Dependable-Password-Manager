e3dbd9a3d1d0a6fbb198b450f0f25145179e997c
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2017-03-03 09:34:48 +0000

    Updated script to configure MySQL server

--- Server/src/main/java/pt/tecnico/sec/dpm/server/db/DPMDB.java
+++ Server/src/main/java/pt/tecnico/sec/dpm/server/db/DPMDB.java
@@ -1,9 +1,9 @@
 package pt.tecnico.sec.dpm.server.db;
 
 public class DPMDB extends DBManager {
-	private static final String DB_URL = "jdbc:mysql://localhost:3306/";
-	private static final String USER = "sec_dpm";
-	private static final String PASS = USER;
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/sec_dpm";
+	private static final String USER = "dpm_account";
+	private static final String PASS = "FDvlalaland129&&";
 	
 	public DPMDB() {
 		super(DB_URL, USER, PASS);
