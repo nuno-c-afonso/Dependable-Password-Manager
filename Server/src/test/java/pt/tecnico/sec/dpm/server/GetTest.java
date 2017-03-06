8a6383f97f4c956c3b002727a361037c08732831
Roberto Ponte <ponte.roberto.cordeiro@gmail.com>
2017-03-06 13:37:23 +0000

    Get test

--- Server/src/test/java/pt/tecnico/sec/dpm/server/GetTest.java
+++ Server/src/test/java/pt/tecnico/sec/dpm/server/GetTest.java
@@ -2,6 +2,8 @@ package pt.tecnico.sec.dpm.server;
 
 import org.junit.*;
 
+import com.mysql.cj.api.jdbc.Statement;
+
 import pt.tecnico.sec.dpm.server.db.DPMDB;
 import pt.tecnico.sec.dpm.server.exceptions.*;
 
@@ -9,9 +11,13 @@ import static org.junit.Assert.*;
 
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
- *  Unit Test example
+ *  Unit Test Get
  *
  *  Invoked by Maven in the "test" life-cycle phase
  *  If necessary, should invoke "mock" remote servers
@@ -19,12 +25,24 @@ import java.security.NoSuchAlgorithmException;
 public class GetTest {
 
     // static members
+	//User information
 	private static byte[] publicKey;
 	final private byte[] USERNAME = "SECUSER".getBytes();
 	final private byte[] DOMAIN = "SECDOMAIN.com".getBytes();
 	
+	//Server API
 	private static APIImpl APIImplTest;
-	private static DPMDB DB;
+	
+	//Database Driver
+	private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	
+	//Connection to the database variable
+	private static Connection conn = null;
+	
+	//Database information
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/sec_dpm";
+	private static final String USER = "dpm_account";
+	private static final String PASS = "FDvlalaland129&&";
 
 
     // one-time initialization and clean-up
@@ -32,21 +50,32 @@ public class GetTest {
     @BeforeClass
     public static void oneTimeSetUp() {
     	APIImplTest = new APIImpl();
-    	DB = new DPMDB();
-    	
-    	
-
+    	// Register driver
+		try {
+			Class.forName(JDBC_DRIVER);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// Open connection
+		try {
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
     @AfterClass
     public static void oneTimeTearDown() {
-    	DB.close();
-
+    	try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
-
-
-    // members
-
 
     // initialization and clean-up for each test
 
@@ -64,14 +93,30 @@ public class GetTest {
     // tests
     //Verifies if the the Put function is working correctly
     @Test
-    public void correctGet() {
+    public void correctGet() throws NoPasswordException {
     	//call function to get
-    	APIImplTest.get(publicKey, DOMAIN, USERNAME);
+    	byte[] password = APIImplTest.get(publicKey, DOMAIN, USERNAME);
     	
-    	//after the return get the result from the DATABASE
+    	byte[] res = null;
+    	Statement stmt = null;
     	
+    	String q = "SELECT password"
+    			 + "FROM passwords"
+    			 + "WHERE publicKey = " +  publicKey 
+    			 + "AND domain = " + DOMAIN 
+    			 + "AND username = " + USERNAME;
     	
-        // assertEquals(expected, actual);
-        // if the assert fails, the test fails
+    	try {
+			stmt = (Statement) conn.createStatement();
+			ResultSet rs = stmt.executeQuery(q);
+			res = rs.getBytes("p.password");
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	assert(password != null);
+    	assert (password == res);
     }
 }
