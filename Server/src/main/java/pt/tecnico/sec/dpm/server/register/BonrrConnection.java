98f7ff65a8bdced9f55c6bad1d7788e940c4c883
RobertoPonte <ponte.roberto.cordeiro@gmail.com>
2017-04-25 17:36:49 +0100

    stuff2

---
+++ Server/src/main/java/pt/tecnico/sec/dpm/server/register/BonrrConnection.java
@@ -0,0 +1,18 @@
+package pt.tecnico.sec.dpm.server.register;
+
+import java.util.List;
+
+public class BonrrConnection implements ByzantineRegisterConnection{
+
+	@Override
+	public List<Object> write() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Object> read() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
