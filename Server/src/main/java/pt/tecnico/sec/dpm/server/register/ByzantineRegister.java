fe082bb6029fc58f2ce7f852ab334366a7ea7f67
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2017-04-25 16:14:10 +0100

    Added ByzantineRegister WSDL

---
+++ Server/src/main/java/pt/tecnico/sec/dpm/server/register/ByzantineRegister.java
@@ -0,0 +1,15 @@
+package pt.tecnico.sec.dpm.server.register;
+
+import java.util.List;
+
+import javax.jws.WebService;
+
+@WebService
+public interface ByzantineRegister {
+	
+	// The return will have: serverCounter + 1, serverSig
+	List<Object> deliverWrite(int serverCounter, int sessionID, int cliCounter, byte[] domain, byte[] username, byte[] password, int wTS, byte[] cliSig, byte[] serverSig);
+	
+	// The return will have: serverCounter + 1, password, wTS, serverSig
+	List<Object> deliverRead(int serverCounter, byte[] cliPublicKey, byte[] domain, byte[] username, byte[] serverSig);
+}
